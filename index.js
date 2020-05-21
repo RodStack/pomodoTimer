e01f58095ef97cd87641cd9f202e3096719a8fec
RodStack <fuenzalida.rodrigo@gmail.com>
2020-05-20 23:23:17 -0400

    1.0

---
+++ index.js
@@ -0,0 +1,62 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime      = document.querySelector('.display__end-time');
+const buttons      = document.querySelectorAll('[data-time]');
+  
+function timer( seconds ) {
+    //clear previous timer
+    clearInterval(countdown);
+
+    const now       = Date.now();
+    const then      = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime( then );
+    
+    
+    countdown = setInterval(()=> {
+        const secondsLeft = Math.round(( then - Date.now() ) / 1000);
+        //stop the interval
+        if( secondsLeft < 0 ){
+            clearInterval(countdown);
+            return;
+        }
+
+ 
+        //Display
+        displayTimeLeft( secondsLeft );
+    }, 1000);
+
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
+
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer() {
+    const seconds= parseInt( this.dataset.time )
+    timer( seconds );
+  }
+
+  //We loop echa buttons and add an event listener 
+buttons.forEach(button => button.addEventListener('click', startTimer));
+
+//We need to add an event listener to the form on the hmtl
+document.customForm.addEventListener( 'submit', function(e){
+    const min       = this.minutes.value;
+    e.preventDefault();
+    timer( min * 60 );
+    this.reset();
+})
